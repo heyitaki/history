28d7ca25df603e5e5b15c1a7fd11276d0bb270d4
Akshath Sivaprasad <akshath@berkeley.edu>
2017-11-15 13:11:42 -0800

    extension is now pretty

--- popup.js
+++ popup.js
@@ -35,6 +35,31 @@ document.addEventListener('DOMContentLoaded', function() {
       'html::-webkit-scrollbar{display:none !important}' +
       'body::-webkit-scrollbar{display:none !important}';
   document.getElementsByTagName('body')[0].appendChild(styleElement);
+
+  // Accordion menu
+  $(function() {
+    var Accordion = function(el, multiple) {
+      this.el = el || {};
+      this.multiple = multiple || false;
+      var links = this.el.find('.link');
+      links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown);
+    };
+
+    Accordion.prototype.dropdown = function(e) {
+      var $el = e.data.el,
+        $this = $(this),
+        $next = $this.next();
+
+      $next.slideToggle();
+      $this.parent().toggleClass('open');
+
+      if (!e.data.multiple) {
+        $el.find('.submenu').not($next).slideUp().parent().removeClass('open');
+      };
+    } 
+
+    var accordion = new Accordion($('.accordion'), false);
+  });
 });
 
 function saveCurrentUrl() {
